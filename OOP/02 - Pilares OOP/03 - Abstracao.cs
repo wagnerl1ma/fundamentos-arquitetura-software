9782dada75c23bd6e11a9b17b4ec2c45821f4120
Wagner Lima <wagner.araujo92@outlook.com>
2022-01-28 21:41:43 -0300

    Adicionar arquivos de projeto.

---
+++ OOP/02 - Pilares OOP/03 - Abstracao.cs
@@ -0,0 +1,23 @@
+﻿namespace OOP
+{
+    public abstract class Eletrodomestico
+    {
+        private readonly string _nome;
+        private readonly int _voltagem;
+        protected Eletrodomestico(string nome, int voltagem)
+        {
+            _nome = nome;
+            _voltagem = voltagem;
+        }
+
+        public abstract void Ligar();
+        public abstract void Desligar();
+        
+
+        // O método Testar() não é obrigado ser implementado, porque já existe uma implementação nesse caso abaixo, porém voce pode modificar esse método usando o "virtual".
+        public virtual void Testar()
+        {
+            // teste do equipamento
+        }
+    }
+}
